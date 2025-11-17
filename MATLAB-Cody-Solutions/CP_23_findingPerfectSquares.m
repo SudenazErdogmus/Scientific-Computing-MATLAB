2c90cbcb9ce77cac267ed3b4fb74f429c049ad7e
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2025-11-17 13:12:34 +0300

    Update CP_23_findingPerfectSquares.m

--- MATLAB-Cody-Solutions/CP_23_findingPerfectSquares.m
+++ MATLAB-Cody-Solutions/CP_23_findingPerfectSquares.m
@@ -6,3 +6,20 @@ Example:
 Output is true since 2^2 is 4 and both 2 and 4 appear on the list.
 %}
 
+function b = isItSquared(a)
+   b=false;
+   i=1; 
+   while(i<=length(a))
+       c= a(i);
+       j=1;
+       while(j<=length(a))
+           if(c^2 == a(j))
+               b=true;
+               return;
+           end
+           j=j+1;
+       end
+       i=i+1;
+   end
+end
+
