4940b758e3c57de8e608cd799df8368c3928bbf2
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2025-11-17 13:04:17 +0300

    Create CP_23_findingPerfectSquares.m

---
+++ MATLAB-Cody-Solutions/CP_23_findingPerfectSquares.m
@@ -0,0 +1,8 @@
+%{
+Given a vector of numbers, return true if one of the numbers is a square of one of the numbers. Otherwise return false.
+Example:
+ Input  a = [2 3 4]
+ Output b is true
+Output is true since 2^2 is 4 and both 2 and 4 appear on the list.
+%}
+
